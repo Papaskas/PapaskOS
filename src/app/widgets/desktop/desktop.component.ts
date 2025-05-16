331e5f093ce7e962a91559405cd44442fcabfab8
Pavel <pavel.glugxix@gmail.com>
2025-05-16 13:51:53 +0300

    Delete dockbar, rename FBI -> papaskOS in angular.json

--- src/app/widgets/desktop/desktop.component.ts
+++ src/app/widgets/desktop/desktop.component.ts
@@ -1,12 +1,11 @@
 import { Component, HostListener } from '@angular/core';
-import { DockBarComponent } from './dock-bar/dock-bar.component';
 import { DesktopShortcutComponent } from '@widgets/desktop/desktop-shortcut/desktop-shortcut.component';
 import { Destinations } from '@app/app.routes';
 import { DesktopBackgroundComponent } from '@widgets/desktop/desktop-background/desktop-background.component';
 
 @Component({
   selector: 'app-desktop',
-  imports: [DesktopShortcutComponent, DockBarComponent, DesktopBackgroundComponent],
+  imports: [DesktopShortcutComponent, DesktopBackgroundComponent],
   templateUrl: './desktop.component.html',
   standalone: true,
   styleUrl: './desktop.component.scss',
